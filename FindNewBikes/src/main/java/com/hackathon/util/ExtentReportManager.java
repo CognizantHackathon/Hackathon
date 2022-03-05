031d915f4558bc390dd049daffd7faaffdda6510
Krishan Chandra <awareness0751@gmail.com>
2022-03-06 00:03:42 +0530

    Added Extent Report file and method

---
+++ FindNewBikes/src/main/java/com/hackathon/util/ExtentReportManager.java
@@ -0,0 +1,33 @@
+package com.hackathon.util;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+public class ExtentReportManager {
+
+	//public static ExtentHtmlReporter htmlReporter;
+	public static ExtentReports report;
+
+	public static ExtentReports getReportInstance() {
+		
+		if( report == null) {
+			ExtentHtmlReporter	htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "\\target\\Report.html");
+			
+		report =new  ExtentReports();
+		report.attachReporter(htmlReporter);
+		
+		report.setSystemInfo("OS", "Windows 10");
+		report.setSystemInfo("Environment", "UAT");
+		report.setSystemInfo("Browser", "Chrome");
+		
+		
+		htmlReporter.config().setDocumentTitle("UAT UI Hackathon Automation Results");
+		htmlReporter.config().setReportName("All Test Cases Report");
+		}
+		
+		
+		
+		return report;
+
+	}
+}
--- FindNewBikes/src/main/java/coms/selenium/hackathon/TestCases.java
+++ FindNewBikes/src/main/java/coms/selenium/hackathon/TestCases.java
@@ -1,13 +1,31 @@
 package coms.selenium.hackathon;
 
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.hackathon.util.ExtentReportManager;
+
 public class TestCases extends BaseUI{
+	ExtentReports report=ExtentReportManager.getReportInstance();
 	@Test
 	public void testOne() {
-		getDriver("mozila");
-		getURL("URL");
-		tearDown();
 		
+		ExtentReports report=ExtentReportManager.getReportInstance();
+		ExtentTest logger = report.createTest("test One");
+		logger.log(Status.INFO, "Open The Browser");
+		getDriver("mozila");
+		logger.log(Status.INFO, "Open The Site URL");
+		getURL("URL");
+		logger.log(Status.INFO, "Quit the Browser Tab");
+		tearDown();
+		logger.log(Status.PASS, "All test cases pass successfully");
+		
+	}
+	@AfterMethod
+	public void endBrowser() {
+		report.flush();
 	}
 }
